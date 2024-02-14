c31f0cb595e28cb908d1253d467a890234e06e83
Haritha982 <114345177+Haritha982@users.noreply.github.com>
2024-02-14 14:23:21 +0530

    Multithreading established

--- BackEnd/dbconncomplete.py
+++ BackEnd/dbconncomplete.py
@@ -4,8 +4,19 @@ import mysql.connector
 import cx_Oracle
 
 def connect_mongodb(host, port, user, password, database):
-    client = pymongo.MongoClient(f"mongodb://{user}:{password}@{host}:{port}/")
-    return client[database]
+    # Use the provided parameters or your connection string
+    # client = pymongo.MongoClient(host)
+    # client = pymongo.MongoClient(f"mongodb://{user}:{password}@{host}:{port}/")
+    client = pymongo.MongoClient("mongodb+srv://dasharitha10:FINT4zAkZ2LYn9EJ@cluster0.g355bm4.mongodb.net/?retryWrites=true&w=majority")
+
+    try:
+        # Check the connection using the ping method
+        client.server_info()
+        print("Connected to MongoDB successfully.")
+    except pymongo.errors.ServerSelectionTimeoutError as e:
+        print("Failed to connect to MongoDB. Error:", e)
+
+    return client
 
 def connect_mysql(host, user, password, database):
     connection = mysql.connector.connect(
@@ -22,7 +33,7 @@ def connect_oracle(host, user, password, database):
     connection = cx_Oracle.connect(user=user, password=password, dsn=dsn)
     return connection
 
-def fetch_table_to_dataframe(connection, table_name):
+def fetch_table_to_dataframe_sql(connection, table_name):
     cursor = connection.cursor()
     query = f"SELECT * FROM {table_name}"
     cursor.execute(query)
@@ -32,19 +43,30 @@ def fetch_table_to_dataframe(connection, table_name):
     df = pd.DataFrame(data, columns=columns)
     return df
 
+def fetch_table_to_dataframe_nosql(connection,database,table):
+    
+    # database_name = "sample_airbnb"
+    # table_name = "listingsAndReviews"
+    db = connection[database]
+    collection = db[table]
+
+    # Fetch data from the collection
+    cursor = collection.find()
+    data = list(cursor)
+
+    # Convert data to Pandas DataFrame
+    df = pd.DataFrame(data)
+
+    # Close the MongoDB connection
+    # connection.close()
+    
+    return df
+
 def gbtodf(db_type,host,user,password,database,table_name):
-    # User input
-    # db_type = input("Enter the database type (mongodb/mysql/oracle): ").lower()
-    # host = input("Enter the host: ")
-    # user = input("Enter the user: ")
-    # password = input("Enter the password: ")
-    # database = input("Enter the database name: ")
-    # table_name = input("Enter the table name: ")
-
-
+    
     # Connect to the selected database
     if db_type == 'MongoDB':
-        connection = connect_mongodb(host, 27017, user, password, database)
+        connection = connect_mongodb(host, 27017, user, password, database)   
     elif db_type == 'MySQL':
         connection = connect_mysql(host, user, password, database)
     elif db_type == 'Oracle DB':
@@ -54,13 +76,11 @@ def gbtodf(db_type,host,user,password,database,table_name):
         return
 
     # Fetch table into a Pandas DataFrame
-    df = fetch_table_to_dataframe(connection, table_name)
+    if db_type == 'MongoDB':
+        df = fetch_table_to_dataframe_nosql(connection, database, table_name)
+    else:
+        df = fetch_table_to_dataframe_sql(connection, table_name)
 
-    # Print the DataFrame
-    # print(df)
-
-    # Close the connection
     connection.close()
+    print(df)
     return df
-
-
--- BackEnd/validation3.py
+++ BackEnd/validation3.py
@@ -1,18 +1,20 @@
+import math
 import pandas as pd
-
-
+# from primary_key import find_primary_key
+from comonPk import get_two_keys
 import logging
 from collections import Counter
-from myapp import *
-from readSouce import *
+import time
+import threading
+from threading import local
 
+thread_local = local()
 # Configure the logging settings with a specific format
 logging.basicConfig(filename='log_file.txt', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Global variables
-
+threadout = [""] * 4
 mappingDoc = {}  # Initialize mappingDoc as an empty dictionary
-
 def substitute_pattern(pattern, row):
     for key, value in row.items():
         pattern = pattern.replace(f"{{{key}}}", str(value))
@@ -25,69 +27,85 @@ def generate_target_data(row, patterns):
     return pd.Series(target_data)
 
 def rowByRowCompare(sourceRow, targetRow, primaryKey):
-    outputString = ""
-    errorCount = 0
+    errorString = ""
 
     for column, sourceValue in sourceRow.items():
         if column != primaryKey:
             targetValue = targetRow[column] if column in targetRow.index else None
+            if str(sourceValue) != str(targetValue):
+                errorString += f">>For {primaryKey}: {sourceRow[primaryKey]}, Column {column} values differ:\n"
+                errorString += f"Source: {sourceValue}\n"
+                errorString += f"Target: {targetValue}\n"
+    
+    # logging.info(f"rowByRowCompare: primaryKey - {primaryKey}, sourceRow - {sourceRow}, targetRow - {targetRow}, errorString - {errorString}")
+    return errorString
 
-            # Check for null values
-            if pd.isnull(sourceValue) or sourceValue == '' or str(sourceValue).strip() == '':
-                if (pd.isnull(sourceValue) or sourceValue == '' or str(sourceValue).strip() == '') and \
-                   (pd.isnull(targetValue) or targetValue == '' or str(targetValue).strip() == ''):
-                    # Both values are null
-                    continue
-                else:
-                    errorCount += 1
-                    outputString += f">> For {primaryKey}: {sourceRow[primaryKey]}, Column {column}\n"
-                    outputString += f"Expected: {sourceValue}\n"
-                    outputString += f"Found: {targetValue}\n"
+def process_rows_dynamic(rows, target_df, mappingDoc, primary_key, thread_index):
+    global threadout
+    local_output_string = []
 
-            # Check for non-null values
-            elif str(sourceValue) != str(targetValue):
-                errorCount += 1
-                outputString += f">> For {primaryKey}: {sourceRow[primaryKey]}, Column {column}\n"
-                outputString += f"Expected: {sourceValue}\n"
-                outputString += f"Found: {targetValue}\n"
+    for _, srcRow in rows.iterrows():
+        transformed_source_row = generate_target_data(srcRow, mappingDoc)
+        primary_key_value = transformed_source_row[primary_key]
+            
+        primary_key_value = target_df[primary_key].values.dtype.type(primary_key_value)
 
-    # logging.info(f"rowByRowCompare: primaryKey - {primaryKey}, sourceRow - {sourceRow}, targetRow - {targetRow}, outputString - {outputString}")
-    return outputString
+        if primary_key_value in target_df[primary_key].values:
+            target_row = target_df[target_df[primary_key] == primary_key_value]
+            result = rowByRowCompare(transformed_source_row, target_row.iloc[0], primary_key)
+            if result:
+                local_output_string.append(result)
+        else:
+            local_output_string.append(f">> Primary key {primary_key_value} not found in target_df")
+    
+    logging.info(f"dividedCompare: primary_key - {primary_key}, outputString - {local_output_string}")
+    # print(f"dividedCompare: primary_key - {primary_key}, outputString - {local_output_string}")
+    # setattr(thread_local, f'outputString_{thread_index}', local_output_string)
+    threadout[thread_index]=local_output_string
+    
 
 def dividedCompare(sourceData, targetData, mappingDoc_input, primary_key):
     mappingDoc = mappingDoc_input
     source_df = sourceData
     target_df = targetData
+    errorCount=0
     logging.info(f"dividedCompare: mappingDoc - {mappingDoc}")
-    logging.info(f"dividedCompare: primary_key used - {primary_key}")
+    start_time = time.time()  # Measure start time
 
-    outputString = []  
+    num_threads = 4
+    chunk_size = math.floor(source_df.shape[0]/num_threads)  # Adjust this based on your requirements
+    
 
-    if source_df.shape[0] == target_df.shape[0]:
-        for _, srcRow in source_df.iterrows():
-            transformed_source_row = generate_target_data(srcRow, mappingDoc)
-            primary_key_value = transformed_source_row[primary_key]
+    threads = []
+    for i in range(num_threads):
+        start_idx = i * chunk_size
+        end_idx = min((i + 1) * chunk_size, len(source_df))
+        thread = threading.Thread(target=process_rows_dynamic, args=(source_df.iloc[start_idx:end_idx], target_df, mappingDoc, primary_key, i))
+        threads.append(thread)
+        thread.start()
 
-            # Convert primary_key_value to the data type of target_df[primary_key].values
-            primary_key_value = target_df[primary_key].values.dtype.type(primary_key_value)
+    # Wait for all threads to finish
+    for thread in threads:
+        thread.join()
 
-            if primary_key_value in target_df[primary_key].values:
-                target_row = target_df[target_df[primary_key] == primary_key_value]
-                result = rowByRowCompare(transformed_source_row, target_row.iloc[0], primary_key)
-                if result:
-                    outputString.append(result)
-            else:
-                outputString.append(f">> Primary key {primary_key_value} not found in target_df")
-            
-        errorCount = ''.join(outputString).count(">>")
-        errornos=[f"Total errors found: {errorCount}\n"]
-        errornos.extend(outputString)
-        outputString=errornos
-        logging.info(f"dividedCompare: primary_key - {primary_key}, outputString - {outputString}, Total errors found: {errorCount}")
+    # Flatten the list of lists
+    flattened_list = [item for sublist in threadout for item in sublist]
 
-    elif source_df.shape[0] < target_df.shape[0]:
-        outputString.append(f"\nNo.of rows of source: {source_df.shape[0]}\nNo.of rows of target: {target_df.shape[0]}\nTarget DataFrame contains duplicate values.")
-    else:
-        outputString.append(f"\nNo.of rows of source: {source_df.shape[0]}\nNo.of rows of target: {target_df.shape[0]}\nValues are missing in the target DataFrame.")
+    # Convert the characters to a string
+    outputString = ''.join(map(str, flattened_list))
 
-    return ''.join(outputString)
+    # Count the occurrences of ">>"
+    errorCount = outputString.count(">>")
+    
+    errornos=f"Total errors found: {errorCount}\n"
+    errornos+= outputString
+    outputString=errornos
+
+# Update outputString with the corrected errornos
+    
+    end_time = time.time()  # Measure end time
+    processing_time = end_time - start_time
+    print(f"Processing time with parallel processing: {processing_time} seconds")
+    print(f"Output string :{outputString} ")
+    logging.info(f"dividedCompare_parallel_dynamic: primary_key - {primary_key}, outputString - {outputString}")
+    return outputString
